ea56909b07cc490237e3095f0ef66237fd02294a
itajacy TI02 <itajacy@bsd.com.br>
2025-11-17 11:23:30 -0300

    aula 14 - visao geral de colecoes

---
+++ src/colecoes/Aula14Collections.java
@@ -0,0 +1,40 @@
+package colecoes;
+
+public class Aula14Collections {
+}
+
+/*
+Aula 14 - Visão geral de Coleções
+
+Caracteristicas das Collections
+
+- Heterogeneas
+- Dinamicas -> podem crescer
+- Objetos
+
+Set
+    - não ordenado (por padrão)
+    - não é indexado
+    - não aceita repetição
+
+List
+    - indexado
+    - aceita repetição
+
+Map
+    - Chave / Valor
+    - Chave não aceita repetição
+    - Valor aceita repetição
+
+Queue(fila)
+    -  implementa fila
+    - First in / First out (FIFO)
+
+Stack(pilha)
+    - implementa pilha
+    - Last in / First out (LIFO)
+
+
+Homogenea => generics
+
+ */
