6055510206b33c7b07f150b306a664a1fa1216ef
itajacy TI02 <itajacy@bsd.com.br>
2025-11-17 11:52:45 -0300

    aula 16 -  aula 2 de Set

---
+++ src/colecoes/ConjuntoComportado.java
@@ -0,0 +1,33 @@
+package colecoes;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class ConjuntoComportado {
+    static void main() {
+        // Set<String> listaAprovados = new HashSet<String>();
+        // Set<String> listaAprovados = new HashSet<>(); // pode só colocar no primeiro a esquerda
+        // Generics 
+        SortedSet<String> listaAprovados = new TreeSet<>(); // lista ordenada com TreeSet
+        listaAprovados.add("Ana");
+        listaAprovados.add("Carlos");
+        listaAprovados.add("Luca");
+        listaAprovados.add("Pedro");
+
+        for (String aprovado : listaAprovados) {
+            IO.println(aprovado);
+        }
+
+        Set<Integer> nums = new HashSet<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(120);
+        nums.add(6);
+
+        for (int n : nums) {
+            IO.println(n);
+        }
+    }
+}
